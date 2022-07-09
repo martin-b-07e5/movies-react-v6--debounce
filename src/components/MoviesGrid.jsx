2bda5832a16b1060b0499c3dc61cec710d9253f7
martin-b-07e5 <martin.bergagno@gmail.com>
2022-07-08 21:59:20 -0300

    error: useEffect must not return anything besides a function, which is used for clean-up. You returned: [object Promise]

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -1,30 +1,11 @@
 // import { useEffect } from "react";
+import { useEffect } from "react";
 import { MovieCard } from "./MovieCard";
 import movies from "./movies.json";
 import styles from "./MoviesGrid.module.css";
 
 // componente para hacer la grilla.
-export function MoviesGrid() {
-  return (
-    <ul className={styles.moviesGrid}>
-      {movies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} />
-      ))}
-    </ul>
-  );
-}
-
-// https://developers.themoviedb.org/3/getting-started/authentication
 // export function MoviesGrid() {
-//   useEffect(() => [
-//     fetch("https://api.themoviedb.org/3/discover/movie", {
-//       headers: {
-//         Authorization:
-//           "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNDIyY2MwMmFmMjkzZDZhMTk3Nzg4ZWVkMjc0YzkzYSIsInN1YiI6IjYyYzM5NjcwM2FmOTI5MDA0YzI5M2RiZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Fh7ApMHwyMoLlp9NbfKwZ-9MNVi8OMR6BUX_b1XzqxU",
-//         "Content-Type": "application/json;charset=utf-8",
-//       },
-//     }),
-//   ]);
 //   return (
 //     <ul className={styles.moviesGrid}>
 //       {movies.map((movie) => (
@@ -33,3 +14,23 @@ export function MoviesGrid() {
 //     </ul>
 //   );
 // }
+
+// https://developers.themoviedb.org/3/getting-started/authentication
+export function MoviesGrid() {
+  useEffect(() => [
+    fetch("https://api.themoviedb.org/3/discover/movie", {
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNDIyY2MwMmFmMjkzZDZhMTk3Nzg4ZWVkMjc0YzkzYSIsInN1YiI6IjYyYzM5NjcwM2FmOTI5MDA0YzI5M2RiZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Fh7ApMHwyMoLlp9NbfKwZ-9MNVi8OMR6BUX_b1XzqxU",
+        "Content-Type": "application/json;charset=utf-8",
+      },
+    }),
+  ]);
+  return (
+    <ul className={styles.moviesGrid}>
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} movie={movie} />
+      ))}
+    </ul>
+  );
+}
