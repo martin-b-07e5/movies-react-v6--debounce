b3d09d27ad6cb8b58f146e8a987502c6a78a55d8
martin-b-07e5 <martin.bergagno@gmail.com>
2022-07-11 08:27:00 -0300

    sharing

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -1,31 +1,35 @@
-// import { useEffect } from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { get } from "../utils/httpClient";
 import { MovieCard } from "./MovieCard";
-import movies from "./movies.json";
 import styles from "./MoviesGrid.module.css";
 
-// componente para hacer la grilla.
-// export function MoviesGrid() {
-//   return (
-//     <ul className={styles.moviesGrid}>
-//       {movies.map((movie) => (
-//         <MovieCard key={movie.id} movie={movie} />
-//       ))}
-//     </ul>
-//   );
-// }
+/* componente para hacer la grilla.
+export function MoviesGrid() {
+  return (
+    <ul className={styles.moviesGrid}>
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} movie={movie} />
+      ))}
+    </ul>
+  );
+} */
 
 // https://developers.themoviedb.org/3/getting-started/authentication
 export function MoviesGrid() {
-  useEffect(() => [
-    fetch("https://api.themoviedb.org/3/discover/movie", {
-      headers: {
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNDIyY2MwMmFmMjkzZDZhMTk3Nzg4ZWVkMjc0YzkzYSIsInN1YiI6IjYyYzM5NjcwM2FmOTI5MDA0YzI5M2RiZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Fh7ApMHwyMoLlp9NbfKwZ-9MNVi8OMR6BUX_b1XzqxU",
-        "Content-Type": "application/json;charset=utf-8",
-      },
-    }),
-  ]);
+  /* // let movies = [];
+  const moviesState = useState([]);
+
+  const movies = moviesState[0];
+  const setMovies = moviesState[1];
+  const [movies, setMovies] = moviesState; */
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    get("/discover/movie").then((data) => {
+      setMovies(data.result);
+    });
+  }, []);
   return (
     <ul className={styles.moviesGrid}>
       {movies.map((movie) => (
